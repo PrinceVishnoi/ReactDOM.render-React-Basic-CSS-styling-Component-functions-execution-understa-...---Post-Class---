324a7ab8943da6f9a9c24079d37e55419a171b1b
Prince Vishnoi <96827891+PrinceVishnoi@users.noreply.github.com>
2022-05-28 18:43:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-ReactDOM.render(
+ReactDOM.render.dom(
   <App />,
   document.getElementById('root')
 );
